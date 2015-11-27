4acfce3dd7526c0110414137da2988f203d8768a
natlehmann <natlehmann@gmail.com>
2015-11-26 22:53:30 -0300

    se agregan estilos al reporte excel

--- src/main/java/com/bmat/digitalcharts/admin/view/ChartSummaryExcelView.java
+++ src/main/java/com/bmat/digitalcharts/admin/view/ChartSummaryExcelView.java
@@ -7,9 +7,13 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.springframework.web.servlet.view.document.AbstractExcelView;
 
 public class ChartSummaryExcelView extends AbstractExcelView {
@@ -19,8 +23,11 @@ public class ChartSummaryExcelView extends AbstractExcelView {
 			HSSFWorkbook workbook, HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		
+        response.setHeader("Content-Disposition", "attachment; filename=\"reporte.xls\"");
+		
 		HSSFSheet excelSheet = workbook.createSheet("Animal List");
-		setExcelHeader(excelSheet);
+		excelSheet.setDefaultColumnWidth(30);         
+		setExcelHeader(excelSheet, workbook);
 		
 //		List animalList = (List) model.get("animalList");
 		setExcelRows(excelSheet, new LinkedList<>());
@@ -28,13 +35,33 @@ public class ChartSummaryExcelView extends AbstractExcelView {
 	}
 	
 	
-	public void setExcelHeader(HSSFSheet excelSheet) {
+	public void setExcelHeader(HSSFSheet excelSheet, HSSFWorkbook workbook) {
+		
+		CellStyle style = workbook.createCellStyle();       
+        Font font = workbook.createFont();       
+        font.setFontName("Arial");        
+        style.setFillForegroundColor(HSSFColor.BLUE.index);      
+        style.setFillPattern(CellStyle.SOLID_FOREGROUND);       
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);       
+        font.setColor(HSSFColor.WHITE.index);      
+        style.setFont(font); 
+        
 		HSSFRow excelHeader = excelSheet.createRow(0);
+		
 		excelHeader.createCell(0).setCellValue("Id");
+		excelHeader.getCell(0).setCellStyle(style);   
+		
 		excelHeader.createCell(1).setCellValue("Name");
+		excelHeader.getCell(1).setCellStyle(style);   
+		
 		excelHeader.createCell(2).setCellValue("Type");
+		excelHeader.getCell(2).setCellStyle(style);   
+		
 		excelHeader.createCell(3).setCellValue("Aggressive");
+		excelHeader.getCell(3).setCellStyle(style);   
+		
 		excelHeader.createCell(4).setCellValue("Weight");
+		excelHeader.getCell(4).setCellStyle(style);   
 	}
 	
 	public void setExcelRows(HSSFSheet excelSheet, List animalList){
