90cbeda8dd80e2d678fb493dd6e82b371c608ecf
natlehmann <natlehmann@gmail.com>
2016-01-05 17:32:11 -0300

    generacion de password para prod

--- src/main/sql/incremental/incremental-2015-11-25.sql
+++ src/main/sql/incremental/incremental-2015-11-25.sql
@@ -24,6 +24,7 @@ create table User_Role(
 INSERT INTO Role (id,name) VALUES (1,'administrador');
 
 INSERT INTO User (id,username,password) VALUES (1,'administrador','42f9d4da1fa9e7e357d9ae38a2bd1afc09f45cbf399f1e54426744f0d8b70528c9e7e9a5bd7aa150');
+--INSERT INTO User (id,username,password) VALUES (1,'Marcelo','0229e71ade9dbe5d2f49ded5b5fc549d7fc41b05d6120e24454c4ed871acc2229e29b6009684f291');
 
 INSERT INTO User_Role (user_id,role_id) VALUES (1,1);
 
