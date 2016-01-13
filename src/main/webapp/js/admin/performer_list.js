ff22b18513a8788eaeda9b074357f9ceb06207f8
natlehmann <natlehmann@gmail.com>
2016-01-13 17:20:23 -0300

    se ordena listado de artistas por nombre por default

--- src/main/webapp/js/admin/performer_list.js
+++ src/main/webapp/js/admin/performer_list.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
         "sPaginationType": "full_numbers",
         "oLanguage": {
             "sUrl": $("#contexto").val() + "js/datatables_ES.txt"
-        }
+        },
+        "aaSorting": [[ 1, "asc" ]]
     } );
     
     setTimeout(function (){
