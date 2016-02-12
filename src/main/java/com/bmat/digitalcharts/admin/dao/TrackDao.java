19e35cbcb0670db7c8bca7f210c23fb8f3d011bc
natlehmann <natlehmann@gmail.com>
2016-02-12 18:50:27 -0300

    se modifica listado de Tracks para que agrupe filas iguales

--- src/main/java/com/bmat/digitalcharts/admin/dao/TrackDao.java
+++ src/main/java/com/bmat/digitalcharts/admin/dao/TrackDao.java
@@ -1,7 +1,9 @@
 package com.bmat.digitalcharts.admin.dao;
 
+import java.math.BigInteger;
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,47 +34,62 @@ public class TrackDao extends AbstractEntityDao<Track> {
 	public List<Track> getAllPaginatedAndFiltered(int inicio, int cantidadResultados,
 			String filtro, String campoOrdenamiento, String direccionOrdenamiento) {
 		
-		if (StringUtils.isEmpty(filtro)) {
-			return super.getAllPaginated(
-					inicio, cantidadResultados, campoOrdenamiento, direccionOrdenamiento);
+		Session session = sessionFactory.getCurrentSession();
+		
+		String queryStr = "SELECT t FROM Track t ";
+		
+		if (!StringUtils.isEmpty(filtro)) {
+			queryStr += "where t.name like :filtro OR t.isrc like :filtro "
+					+ "OR t.song.name like :filtro OR t.performer.name like :filtro "
+					+ "OR t.release.labelCompany.name like :filtro ";
+		}
+		
+		queryStr += "GROUP BY t.isrc, t.release.id, t.performer.id, t.song.id ";
+		
+		if ( !StringUtils.isEmpty(campoOrdenamiento) ) {
+			queryStr += " order by " + campoOrdenamiento + " " + direccionOrdenamiento;
+		}
+		
+		
+		Query query = session.createQuery(queryStr);
+		
+		if (!StringUtils.isEmpty(filtro)) {
+			query.setParameter("filtro", "%" + filtro + "%");
+		}
 			
-		} else {
-			
-			Session session = sessionFactory.getCurrentSession();
-			
-			String query = "from Track where name like :filtro OR isrc like :filtro "
-					+ "OR song.name like :filtro OR performer.name like :filtro";
-			
-			if ( !StringUtils.isEmpty(campoOrdenamiento) ) {
-				query += " order by " + campoOrdenamiento + " " + direccionOrdenamiento;
-			}
-			
-			return session.createQuery(query)
-					.setParameter("filtro", "%" + filtro + "%")
-					.setFirstResult(inicio)
+		return query.setFirstResult(inicio)
 					.setMaxResults(cantidadResultados).list();
 			
-		}
 	}
 
 	@Transactional(value="transactionManager")
 	public Long getCount(String filtro) {
 		
-		if (StringUtils.isEmpty(filtro)) {
-			return super.getCount();
-			
-		} else {
-			
-			Session session = sessionFactory.getCurrentSession();
-			
-			String query = "select count(e) from Track e where e.name like :filtro OR e.isrc like :filtro "
-					+ "OR e.song.name like :filtro OR performer.name like :filtro";
-			
-			Long resultado = (Long) session.createQuery(query)
-					.setParameter("filtro", "%" + filtro + "%").uniqueResult();
-			
-			return resultado != null ? resultado.longValue() : 0;
+		Session session = sessionFactory.getCurrentSession();
+
+
+		String queryStr = "select count(1) FROM ("
+				+ "select 1 from Track t, Song s, Performer p, BMATDigitalChartsDB.Release r, LabelCompany l "
+				+ "WHERE t.songId = s.id AND t.performerId = p.id "
+				+ "AND t.releaseId = r.id AND r.labelCompanyId = l.id ";
+		
+		if (!StringUtils.isEmpty(filtro)) {
+			queryStr += "AND (t.name like :filtro OR t.isrc like :filtro "
+					+ "OR s.name like :filtro OR p.name like :filtro "
+					+ "OR l.name like :filtro) ";
 		}
+		
+		queryStr += "group by t.isrc, t.releaseId, t.performerId, t.songId) as tmp";
+		
+		Query query = session.createSQLQuery(queryStr);
+		
+		if (!StringUtils.isEmpty(filtro)) {
+			query.setParameter("filtro", "%" + filtro + "%");
+		}
+			
+		BigInteger resultado = (BigInteger) query.uniqueResult();
+			
+		return resultado != null ? resultado.longValue() : 0;
 	}
 
 }
